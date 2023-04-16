8fbe96009ec841226e3eab98d40b1e7b646e369b
sahil gholap <gholapsahil007@gmail.com>
2023-04-16 19:08:33 +0530

    commit

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "https://threejs-openai-project.onrender.com/api/v1/dalle",
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
